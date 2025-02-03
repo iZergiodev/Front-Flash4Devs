2667625c504948c14290e28825aa4f21fd2cbb14
izergio98 <izergiodev@gmail.com>
2025-02-03 22:24:05 +0100

    vercel fix

--- src/auth/router/AuthRouter.jsx
+++ src/auth/router/AuthRouter.jsx
@@ -1,14 +1,16 @@
 import { Route, Routes } from "react-router"
-import { Home } from "../views/Home.jsx";
 import { Login } from "../views/Login.jsx"
 import { Register } from "../views/Register.jsx"
 
 export const AuthRouter = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
+      <Route path="/" element={<Login />} />
+
+      <Route path="login" element={<Login />} />
+      <Route path="register" element={<Register />} />
+
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
